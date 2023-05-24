178747099a1c9ac51404658234c987a3de9153cf
Ruorong Qi <129242594+qiruorong@users.noreply.github.com>
2023-05-23 23:10:15 -0400

    Create NestedForLoopNum.java

---
+++ NestedForLoopNum.java
@@ -0,0 +1,10 @@
+public class NestedForLoop {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 7; i++) {
+            for (int j = 1; j <= 1; j++) {
+                System.out.print(" ");
+            }
+            System.out.println();
+        }
+    }
+}
