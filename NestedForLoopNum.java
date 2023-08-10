c3af5d7b45f684dede5b6784a9e2455f04447332
Ruorong Qi <129242594+qiruorong@users.noreply.github.com>
2023-08-10 17:12:19 -0400

    Update NestedForLoopNum.java

--- NestedForLoopNum.java
+++ NestedForLoopNum.java
@@ -1,8 +1,8 @@
 public class NestedForLoop {
     public static void main(String[] args) {
-        for (int i = 1; i <= 7; i++) {
+        for (int i = 1; i <= 5; i++) {
             for (int j = 1; j <= i; j++) {
-                System.out.print(i + " ");
+                System.out.print("*");
             }
             System.out.println();
         }
