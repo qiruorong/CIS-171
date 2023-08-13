d4a9bb37ce21ecff314682208b50b8ab3f3018c0
Ruorong Qi <129242594+qiruorong@users.noreply.github.com>
2023-08-13 12:50:47 -0400

    Update notes.java

--- notes.java
+++ notes.java
@@ -67,10 +67,95 @@ for (int line=1; line<=3; line++) { //12345, 12345, 12345 in each line
   }
 
 
-for (int i=0; i<=9; i++) {
-  for (int j=1; j<=3; j++) {
-    System.out.print(j);
-  System.out.print(i);
+for (int k=1; k<=3; k++) {  //000111222333444555666777888999 三行
+    for (int i=0; i<=9; i++) {
+      for (int j=1; j<=3; j++) {
+        System.out.print(i);
+      }
+    }
+System.out.println();
+}
+  
+
+for (int line = 1; line <= 6; line++) {  //123456斜着diagonal
+  for (int j = 0; j <= 6 - line; j++) {
+    System.out.print(" ");
+  }
+  System.out.println(line);
+}
+
+// public static final int LINE = 6, 可以把下面一行的6 替换成LINE
+for (int line = 1; line <= 6; line++) { // 1 22 333 444 555靠右
+    for (int j = 0; j <= 7-line; j++) {
+        System.out.print(" ");
+    }
+    for (int i = 1; i <= line; i++) {
+        System.out.print(line);
+    }
+    System.out.println();
+}
 
 
+// number = number*2
+number *= 2
+// print x
+System.out.println("X = " + X)
 
+
+// 输入公式 
+public class Rectangle {
+  public static void main(String[] args) {
+      double length = 1;
+      double width = 2;
+      rectangle(length, width);
+  }
+  public static void rectangle(double length, double width) {
+      System.out.println("area = " + length * width);
+      System.out.println("parameter = " + length*2 + width*2);
+    }
+}
+
+! use Math.PI for 3.1415926
+! use Math.pow (radius, 2) for square of radius
+
+if using a parameter that is not given an number but a calculation based on given num, use double x = a*b*c under line#112
+
+
+//method overloading: line#138
+can define multiple method with same name as long as they don't have same num or not same type
+for example:
+public class Rectangle {
+  public static void main(String[] args) {
+    double length = 1;
+    double width = 2;
+    rectangle(length, width);
+  }
+  public static void rectangle(double length, double width) {
+    System.out.println("area = " + length * width);
+    System.out.println("parameter = " + length*2 + width*2);
+  }
+  public static void rectangle(double length, double width) {
+    double width = 4; ////!!!!!!!!!!!!! THIS STEP IS METHOD OVERLOADING
+    System.out.println("area = " + length * width);
+    System.out.println("parameter = " + length*2 + width*2);
+  }
+}
+
+
+// void: don''t return any value, so if:
+return X 
+// have to change public static void into:
+public static double X or whatever data type you have
+// for example:
+public class printRectangle {
+  public static void main(String[] args) {
+    double length = 3;
+    double width = 3.5;
+    System.out.println("Area = " + rectangle(length, width));
+  }
+
+  public static double rectangle(double length, double width) { // same as #112
+    double res = length * width;
+    return res;
+  }
+}
