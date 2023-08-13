84cdfb3596124d1cd9e84f87cd7c3fa8b80dd34d
Ruorong Qi <129242594+qiruorong@users.noreply.github.com>
2023-08-13 14:16:29 -0400

    Update notes.java

--- notes.java
+++ notes.java
@@ -1,99 +1,18 @@
-// HelloWorld code
-Public class Helloworld { //file name
-  public static void main(String[] args) { //like "def" in python
-    System.out.printLn("HelloWorld!"); //print Line
-    System.out.print("HelloWorld!"); //doesn't print on new line
-    System.out.printLn("Sky is 'blue' and \"life\" is {beautiful}"); //Sky is 'blue' and "life" is {beautiful}
-    System.out.printLn("Sky is blue and \tlife is beautiful"); //tab
-    System.out.printLn("Sky is blue and \"life is beautiful"); //need a backslash to print whatever signal after it
-    System.out.printLn("Sky is blue and \nlife is beautiful"); //new line
-  }
-}
+Chapter 1:
+VictoryIsMine;
+PatternGenerator;
+
+Chapter 2:
+NestedForLoop;
+NestedForLoopNum;
 
 
-// Computer binary
-// 8 bit = 1 byte
-// 1024 byte = 1 KiloByte, MegaByte, GigaByte, TeraByte, PetaByte, ExaByte, ZettaByte, YottaByte
-
-// 0=0, 1=1, 2=10, 3=11, 4=100, 5=10
-// divide the number by 2, until you got 0/1, and put each remain from bottom to top
-
-// hexadecimal number
-// 0-9, A-F
-
-// octal number
-// 0-7
-
-// Class name: ThisIsMyVariable
-// Variable name: thisIsMyVariable (cp
-
-
-Public class Figure2 {
-  public static void main(String[] args) {
-    drawX;
-    drawDiamond;
-  }
-  public static void drawDiamond() {
-    System.out.printLn("    /\\  ");
-    System.out.printLn("   /  \\ ");
-    System.out.printLn("   \\  / ");
-    System.out.printLn("    \\/  ");
-  }
-  Public static vois drawX() {
-    System.out.printLn("   \\  / ");
-    System.out.printLn("    \\/  ");
-    System.out.printLn("     /\\ ");
-    System.out.printLn("    /  \\ ");
-  }
-}
-
-/// int, double, char, boolean(True/False)
-System.out.println(5/3); // 1
-System.out.println(5.0/3); // 1.666667
-System.out.println(5%3); //2 (modulo operator return reminder)
-System.out.println(4%10); //4 (the original num)
-
-
-// for loop
-for (int line=1; line<=5; line++) { //11 22 33 44 55 in each line
-  for (int i=1; i<=5; i++) {
-    System.out.print(line); //use print to print in a line (correspond with second line code)
-  }
-  System.out.println( ); //use println to print in a new line (correspond with first line code)
-}
-for (int line=1; line<=3; line++) { //12345, 12345, 12345 in each line
-  for (int j=5; i>=1; j--) {
-    System.out.println(line);
-  }
-
-
-for (int k=1; k<=3; k++) {  //000111222333444555666777888999 三行
-    for (int i=0; i<=9; i++) {
-      for (int j=1; j<=3; j++) {
-        System.out.print(i);
-      }
-    }
-System.out.println();
-}
-  
-
-for (int line = 1; line <= 6; line++) {  //123456斜着diagonal
-  for (int j = 0; j <= 6 - line; j++) {
-    System.out.print(" ");
-  }
-  System.out.println(line);
-}
-
-// public static final int LINE = 6, 可以把下面一行的6 替换成LINE
-for (int line = 1; line <= 6; line++) { // 1 22 333 444 555靠右
-    for (int j = 0; j <= 7-line; j++) {
-        System.out.print(" ");
-    }
-    for (int i = 1; i <= line; i++) {
-        System.out.print(line);
-    }
-    System.out.println();
-}
+// refactorying: make code more efficient
+NOTES between chap 2 and chap 3: 
+formula;
+Math.PI, Math.pow;
+methodOverloading;
+voidReturn;
 
 
 // number = number*2
@@ -121,7 +40,7 @@ public class Rectangle {
 if using a parameter that is not given an number but a calculation based on given num, use double x = a*b*c under line#112
 
 
-//method overloading: line#138
+//method overloading: 
 can define multiple method with same name as long as they don't have same num or not same type
 for example:
 public class Rectangle {
@@ -154,8 +73,49 @@ public class printRectangle {
     System.out.println("Area = " + rectangle(length, width));
   }
 
-  public static double rectangle(double length, double width) { // same as #112
-    double res = length * width;
+  public static double rectangle(double length, double width) { // same as #56
+    double res = length * width; // or return = length * width to replace #77&78
     return res;
   }
 }
+
+
+//adding from 1-100
+public class CalculateSum {
+    public static void main(String[] args) {
+        int n = calculateSum(100);
+        System.out.println("Sum = " + n);
+    }
+    public static int calculateSum(int n) {
+        int sum = 0; // initialize sum = 0
+        for (int i = 1; i <= n; i++) { // or sum = n * (n+1)/2.0
+            sum += i; // or sum = sum + 1
+        }
+        return sum;
+    }
+}
+
+
+// index number
+public class StringObject {
+  public static void main (String[] args) {
+    String s = "Hello there";
+    System.out.println(s.chatAt(0)); // print out H
+    for (int i=0; i<s; i++) // note not the = sign here
+      // for (int i=0; i<=s.length()-1; i++)
+      System.out.println(s.chatAt(i)); // print each 字母 in each line
+    System.out.println("\n" + s.indexOf("l")); // print 2
+    System.out.println(s.replace ("l", "y")); //print Heyyo there
+    System.out.println(s.toUpperCase); 
+    System.out.println(s.toLowerCase);
+    
+    
+
+
+
+Chapter 3:
+PowerOf2;
+ProcessName;
+
+Chapter 3g:
+
