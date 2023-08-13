0e180cc93959ea32618a6b2cb24e7b8c498fa4d9
Ruorong Qi <129242594+qiruorong@users.noreply.github.com>
2023-08-13 12:51:34 -0400

    Update VictoryIsMine.java

--- VictoryIsMine.java
+++ VictoryIsMine.java
@@ -5,3 +5,37 @@ public class VictoryIsMine {
         System.out.println("\\ \\ \\ \\ \\ \\ \\ \\ \\ \\ \\ \\ \\ \\ \\ \\ \\ \\ \\ \\ \\");
     }
 }
+
+
+
+
+
+NOTES:
+Public class Helloworld { //file name
+  public static void main(String[] args) { //like "def" in python
+    System.out.printLn("HelloWorld!"); //print Line
+    System.out.print("HelloWorld!"); //doesn't print on new line
+    System.out.printLn("Sky is 'blue' and \"life\" is {beautiful}"); //Sky is 'blue' and "life" is {beautiful}
+    System.out.printLn("Sky is blue and \tlife is beautiful"); //tab
+    System.out.printLn("Sky is blue and \"life is beautiful"); //need a backslash to print whatever signal after it
+    System.out.printLn("Sky is blue and \nlife is beautiful"); //new line
+  }
+}
+
+
+// Computer binary
+// 8 bit = 1 byte
+// 1024 byte = 1 KiloByte, MegaByte, GigaByte, TeraByte, PetaByte, ExaByte, ZettaByte, YottaByte
+
+// 0=0, 1=1, 2=10, 3=11, 4=100, 5=10
+// divide the number by 2, until you got 0/1, and put each remain from bottom to top
+
+// hexadecimal number
+// 0-9, A-F
+
+// octal number
+// 0-7
+
+// Class name: ThisIsMyVariable
+// Variable name: thisIsMyVariable (cp
+
