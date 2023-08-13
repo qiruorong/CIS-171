f562763cdcadb241e086967a9e99f2ed6fda47fe
Ruorong Qi <129242594+qiruorong@users.noreply.github.com>
2023-08-13 12:53:17 -0400

    Update NestedForLoopNum.java

--- NestedForLoopNum.java
+++ NestedForLoopNum.java
@@ -8,3 +8,25 @@ public class NestedForLoop {
         }
     }
 }
+
+
+
+
+NOTES:
+for (int line = 1; line <= 6; line++) {  //123456斜着diagonal
+  for (int j = 0; j <= 6 - line; j++) {
+    System.out.print(" ");
+  }
+  System.out.println(line);
+}
+
+// public static final int LINE = 6, 可以把下面一行的6 替换成LINE
+for (int line = 1; line <= 6; line++) { // 1 22 333 444 555靠右
+    for (int j = 0; j <= 7-line; j++) {
+        System.out.print(" ");
+    }
+    for (int i = 1; i <= line; i++) {
+        System.out.print(line);
+    }
+    System.out.println();
+}
