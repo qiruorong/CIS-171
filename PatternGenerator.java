54078fd2d761be6c8b8cf263d242d9c4a1584faa
Ruorong Qi <129242594+qiruorong@users.noreply.github.com>
2023-08-13 12:52:11 -0400

    Update PatternGenerator.java

--- PatternGenerator.java
+++ PatternGenerator.java
@@ -47,3 +47,33 @@ public class PatternGenerator {
         System.out.println("    *");
     }
 }
+
+
+
+
+
+NOTES:
+Public class Figure2 {
+  public static void main(String[] args) {
+    drawX;
+    drawDiamond;
+  }
+  public static void drawDiamond() {
+    System.out.printLn("    /\\  ");
+    System.out.printLn("   /  \\ ");
+    System.out.printLn("   \\  / ");
+    System.out.printLn("    \\/  ");
+  }
+  Public static vois drawX() {
+    System.out.printLn("   \\  / ");
+    System.out.printLn("    \\/  ");
+    System.out.printLn("     /\\ ");
+    System.out.printLn("    /  \\ ");
+  }
+}
+
+/// int, double, char, boolean(True/False)
+System.out.println(5/3); // 1
+System.out.println(5.0/3); // 1.666667
+System.out.println(5%3); //2 (modulo operator return reminder)
+System.out.println(4%10); //4 (the original num)
