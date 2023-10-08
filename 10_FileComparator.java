298d814fb38c14887efbb5b3b8644bf8cc6d8688
Ruorong Qi <129242594+qiruorong@users.noreply.github.com>
2023-10-08 15:50:55 -0400

    Update 10_FileComparator.java

--- 10_FileComparator.java
+++ 10_FileComparator.java
@@ -1,19 +1,20 @@
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.IOException;
+import java.io.*;
+import java.util.*;
 
 public class FileComparator {
-    public static void main(String[] args) {
-        try (BufferedReader reader1 = new BufferedReader(new FileReader("data1.txt"));
-             BufferedReader reader2 = new BufferedReader(new FileReader("data2.txt"))) {
+    public static void main (String[] args) throws FileNotFoundException {
+        System.out.prinln ("This program will add a series of numbers from a file.");
+        System.out.println();
+        Scanner console = new Scanner(System.in);
+        System.out.print("What is the file name?");
+        Stirng name = console.nextLine();
+        Scanner input = new Scanner(new File(name));
+        System.out.println();
 
-            String line1, line2;
-            int lineNum = 0;
-            boolean differencesFound = false;
+        int count = 0;
+           
 
-            while ((line1 = reader1.readLine()) != null && (line2 = reader2.readLine()) != null) {
-                lineNum++;
-
+            
                 if (!line1.equals(line2)) {
                     differencesFound = true;
                     System.out.println("Difference found:");
@@ -49,3 +50,90 @@ public class FileComparator {
         }
     }
 }
+
+
+
+
+
+
+
+
+
+
+import java.io.*;// for file class
+import java.util,*
+public class FileProcessing {
+    public static void main(Sting[] args) {
+        File f = new File ("hamlet.txt");
+        System.out.println("exist?: " + f.exists());
+        System.out.println("can Read?: " + f.canRead());
+    }
+}
+
+
+import java.io.*;// for file class
+import java.util.*;
+public class CountWords2 {
+    public static void main (String[] args) throws FileNotFoundException {
+        Scanner in = new Scanner(new File("hamlet.txt"));
+        int count = 0;
+        while (in.hasNext()) { //if file read, goes into while loop
+            string word = in.next();
+            count++;
+        }
+        System.out.prinln("Total words = " + count);
+        }
+    }
+}
+    
+
+// open & read txt, count #letter in txt
+import java.io.*;
+import java.util.*;
+public class CountWords2 {
+    public static void main (String[] args) throws FileNotFoundException {
+        Scanner in = new Scanner(new File("hamlet.txt"));
+        int count = 0;
+        int totalletters = 0;
+        while(in.hasNext()) {
+            String word = in.mext();
+            totalLetters += word.length();
+            count++;
+        }
+        System.out.println("Total words = " + count);
+    }
+}
+
+
+// input file name, fine Min&Max
+import java.io.*;
+import java.util.*;
+public class ShowSum4 {
+    public static void main (String[] args) throws FileNotFoundException {
+        System.out.prinln ("This program will add a series of numbers from a file.");
+        System.out.println();
+        Scanner console = new Scanner(System.in);
+        System.out.print("What is the file name?");
+        Stirng name = console.nextLine();
+        Scanner input = new Scanner(new File(name));
+        System.out.println();
+
+        double sum = 0.0;
+        int count = 0;
+        double min, max;
+        min = max = input.nextDouble();
+        while (input.hasNextDouble()) {
+            double next = input.nextDouble();
+            if(min > next) min = next;
+            if(max < next) max = next;
+            count++;
+            System.out.println("number " + count + " = " + next);
+            sum += next;
+        }
+        System.out.println("Min = " + min);
+        System.out.println("Max = " + max);
+    }
+}
+
+
+
